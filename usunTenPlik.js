99859c336632a58f05b97754b5eefdd4cad6e755
Dariusz Nowak <dariusz.nowak012@gmail.com>
2022-08-29 02:14:25 +0200

    Add redux toolkit

---
+++ usunTenPlik.js
@@ -0,0 +1,25 @@
+const initialState = {
+  boards: [
+    {
+      id: 12,
+      title: "To do",
+      tasks: [
+        { id: 21, text: "Select background colour" },
+        { id: 52, text: "Select background image from Unsplash" },
+      ],
+    },
+    {
+      id: 35,
+      title: "In progress",
+      tasks: [
+        { id: 28, text: "Fix CSS issues" },
+        { id: 18, text: "Add drag-and-drop functionality" },
+      ],
+    },
+    {
+      id: 14,
+      title: "Completed",
+      tasks: [{ id: 1, text: "Use Redux Toolkit for state management" }],
+    },
+  ],
+};
